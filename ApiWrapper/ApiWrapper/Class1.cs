fe11a0115c3544e1e3aba2624e12af2491d0b516
Michael Sundman <sundman@kth.se>
2013-02-02 10:24:02 +0100

    apiwrapper

---
+++ ApiWrapper/ApiWrapper/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace ApiWrapper
+{
+    public class ApiWrapper
+    {
+        static string addr = "https://lostinspace.lanemarknad.se:8000/api2";
+        private static int port = 8000;
+        private static string apiKey = "7bcd9893-bb21-464f-b4a7-48a8a8663334";
+        private static string session = "3af98f60-0a24-4867-a889-71c1b878eab1";
+
+
+        public static JObject GetData(string command, Dictionary<string,string> vars = null  )
+        {
+            string uriString = addr;
+
+            uriString += "/?session=" + session;
+            if(vars != null)
+            foreach (KeyValuePair<string, string> kvp in vars)
+            {
+                uriString += "&" + kvp.Key + "=" + kvp.Value;
+            }
+
+            uriString += "&command=" + command;
+            
+            Uri uri = new Uri(addr);
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            request.CookieContainer.Add(uri, new Cookie("devnull_apikey", apiKey));
+            request.CookieContainer.Add(uri, new Cookie("devnull_session", session));
+
+
+
+            var response = request.GetResponse();
+
+            Stream resStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(resStream);
+            return JObject.Parse(reader.ReadToEnd());
+        }
+    }
+}
--- TestEmptyConsoleApp/TestEmptyConsoleApp/Program.cs
+++ TestEmptyConsoleApp/TestEmptyConsoleApp/Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +13,18 @@ namespace TestEmptyConsoleApp
     {
         static void Main(string[] args)
         {
+
+            string text = @"{""stars"":[{""x"":""100"",""y"":""100"",""class"":""M4.5V"",""planets"":""8"",""name"":""Ross 614""},{""x"":""80"",""y"":""30"",""class"":""M3.0V"",""planets"":""5"",""name"":""Wolf 1061""},{""x"":""10"",""y"":""160"",""class"":""DZ7"",""planets"":""8"",""name"":""Van Maanen's star""},{""x"":""140"",""y"":""170"",""class"":""M3.0V"",""planets"":""12"",""name"":""Gliese 1""},{""x"":""142"",""y"":""77"",""class"":""M4.5V"",""planets"":""6"",""name"":""TZ Arietis""},{""x"":""80"",""y"":""120"",""class"":""M3.0V"",""planets"":""3"",""name"":""Gliese 687""},{""x"":""180"",""y"":""28"",""class"":""M4.5V"",""planets"":""4"",""name"":""Gliese 876""},{""x"":""42"",""y"":""180"",""class"":""M2.0V"",""planets"":""5"",""name"":""Lalande 21185""},{""x"":""50"",""y"":""60"",""class"":""K2V"",""planets"":""10"",""name"":""Epsilon Eridani""},{""x"":""170"",""y"":""166"",""class"":""M4.5V"",""planets"":""8"",""name"":""Kapteyn's star""},{""x"":""70"",""y"":""190"",""class"":""M0.0V"",""planets"":""1"",""name"":""Lacaille 8760""}]}";
+            var jsreader = new JsonTextReader(new StringReader(text));
+            var json = (JObject)new JsonSerializer().Deserialize(jsreader);
+            var stars = from p in json["stars"]
+                 select new
+                 {
+                     Class = (string)p["class"],
+                     Planets = (int)p["planets"],
+                     X = (int)p["x"],
+                     Y = (int)p["y"],
+                 };
         }
     }
 }
